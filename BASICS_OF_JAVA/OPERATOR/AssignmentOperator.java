fcd740c5b632964f0f6837cc243e2dc069df9e21
Saqib <mohd.22scse1310007@galgotiasuniversity.edu.in>
2023-11-04 00:56:39 +0530

    add assignment operator file

---
+++ BASICS_OF_JAVA/OPERATOR/AssignmentOperator.java
@@ -0,0 +1,15 @@
+package BASICS_OF_JAVA.OPERATOR;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        /*
+        
+        1- = 
+        2- +=
+        3- -=
+        4- *=
+        5- /=
+                 
+        */
+    }
+}
