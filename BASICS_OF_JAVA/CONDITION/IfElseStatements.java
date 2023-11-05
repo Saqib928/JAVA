b1e8ec2a8dad3377e4271cba4d69803a45c4a606
Saqib <mohd.22scse1310007@galgotiasuniversity.edu.in>
2023-11-06 00:39:48 +0530

    add new file name if else

---
+++ BASICS_OF_JAVA/CONDITION/IfElseStatements.java
@@ -0,0 +1,20 @@
+package BASICS_OF_JAVA.CONDITION;
+
+import java.util.Scanner;
+
+public class IfElseStatements {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if (age>=18){
+            System.out.println("Adult : drive,vote");
+        }
+        else if (age>13 || age <18){
+            System.out.println("Teenager");
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+    }
+    
+}
