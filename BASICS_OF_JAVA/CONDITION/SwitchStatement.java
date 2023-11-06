33287c836df4922a7b8ea3db4944415b142e5643
Saqib <mohd.22scse1310007@galgotiasuniversity.edu.in>
2023-11-07 01:40:33 +0530

    add file calculator using switch case

---
+++ BASICS_OF_JAVA/CONDITION/SwitchStatement.java
@@ -0,0 +1,24 @@
+package BASICS_OF_JAVA.CONDITION;
+import java.util.Scanner;
+public class SwitchStatement {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number :");
+        int a = sc.nextInt();
+        System.out.println("Enter second number :");
+        int b = sc.nextInt();
+        System.out.println("Enter the Operator : ");
+        char Operator = sc.next().charAt(0);
+        switch (Operator){
+            case '+' : System.out.println(a+b);
+            break;
+            case '-' : System.out.println(a-b);
+            break;
+            case '*' : System.out.println(a*b);
+            break;
+            case '/' : System.out.println(a/b);
+            break;
+
+        }
+    }
+}
