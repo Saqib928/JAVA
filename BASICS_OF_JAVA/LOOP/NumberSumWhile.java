d8d53c48b5ed607ae14781b94c457f790b21f6f1
Saqib <mohd.22scse1310007@galgotiasuniversity.edu.in>
2023-11-07 19:16:55 +0530

    add sum of n number file

---
+++ BASICS_OF_JAVA/LOOP/NumberSumWhile.java
@@ -0,0 +1,16 @@
+package BASICS_OF_JAVA.LOOP;
+import java.util.Scanner;
+public class NumberSumWhile {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value till print Sum of Natural Number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+        while(i<=n){
+            sum = sum+i;
+            i++;
+        }
+        System.out.println("Sum of number till " + n +" is "+ sum);
+    }
+}
