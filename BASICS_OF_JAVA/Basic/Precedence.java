fba14756a201dd520f789d84355b11191e6cefde
saqib928 <mohd.22scse1310007@galgotiasuniversity.edu.in>
2024-01-17 22:39:33 +0530

    add a file in the java program file for precedence

---
+++ BASICS_OF_JAVA/Basic/Precedence.java
@@ -0,0 +1,47 @@
+package BASICS_OF_JAVA.Basic;
+
+public class Precedence {
+    public static void main(String[] args) {
+        /*
+        Postfix: expr++, expr--
+Unary: ++expr, --expr, +expr, -expr, ~, !
+Multiplicative: *, /, %
+Additive: +, -
+Shift: <<, >>, >>>
+Relational: <, <=, >, >=, instanceof
+Equality: ==, !=
+Bitwise AND: &
+Bitwise XOR: ^
+Bitwise OR: |
+Logical AND: &&
+Logical OR: ||
+Conditional (Ternary): ? :
+Assignment and compound assignment operators
+
+Precedence:
+
+++ --
++ - ! ~
+* / %
++ -
+<< >> >>>
+< <= > >= instanceof
+== !=
+&
+^
+|
+&&
+||
+? :
+= += -= *= /= %= &= ^= |= <<= >>= >>>=
+Associativity:
+
+Left-associative: ++ -- + - * / % << >> >>> < <= > >= == != & ^ | && || ? : = += -= *= /= %= &= ^= |= <<= >>= >>>=
+Right-associative: = += -= *= /= %= &= ^= |= <<= >>= >>>=
+         */int a = 5;
+         int b = 7;
+         int c = 12;
+         int factor = b*b-(4*a*c)/(2*a);
+         System.out.println("the factor for given expression is : "+ factor);
+    }
+}
