1e4902eecb8547f6ebeb7dcc0e9b1dad5e4bf6e9
saqib928 <mohd.22scse1310007@galgotiasuniversity.edu.in>
2024-01-19 23:58:44 +0530

    small change in repo

--- Basic_Question_Practice/question1.java
+++ Basic_Question_Practice/question1.java
@@ -2,6 +2,7 @@ package Basic_Question_Practice;
 
 public class question1 {
     public static void main(String[] args) {
+        //This is the code for understanding of datatype
         float C = 7/4 * 9/2;
         System.out.println("Wrong answer because divisor type is int : "+ C);
         float B = 7/4.0f * 9/2.0f;
