023d514ba01c4e6cdee91f309ebfbb152ab3e760
saqib928 <mohd.22scse1310007@galgotiasuniversity.edu.in>
2024-01-20 23:55:55 +0530

    main nhi bataoga

--- Basic_Question_Practice/question1.java
+++ Basic_Question_Practice/question1.java
@@ -1,11 +1,19 @@
 package Basic_Question_Practice;
-
+import java.util.Scanner;
 public class question1 {
     public static void main(String[] args) {
-        //This is the code for understanding of datatype
-        float C = 7/4 * 9/2;
+        Scanner sc = new Scanner (System.in);
+        int a,b,c,d;
+        System.out.print("insert the four for operation : ");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        d=sc.nextInt();
+        float C = a/c * b/d;
         System.out.println("Wrong answer because divisor type is int : "+ C);
-        float B = 7/4.0f * 9/2.0f;
+        float B = 7/(float)c * 9/(float)d;
         System.out.println("Right Answer because divisor type is float and result store in float : "+B);
+        sc.close();
     }
+   
 }
